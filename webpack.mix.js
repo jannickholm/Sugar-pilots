a22448b2698b75539ab894ee91d51cfb217f2516
Jannick Holm <jannickholm@users.noreply.github.com>
2020-04-17 20:50:56 +0200

    Fix compiler and system.yalm

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,5 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('site/themes/sugarpilots/js/site.js', 'sugarpilots/js/sugarpilots.js')
+mix.js('site/themes/sugarpilots/js/site.js', 'site/themes/sugarpilots/js/sugarpilots.js')
     .sass('site/themes/sugarpilots/sass/sugarpilots.scss', 'site/themes/sugarpilots/css');
