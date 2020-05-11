1c24a7b08e8e839400893a0c17939cd899a9e16d
Jannick Holm <jannickholm@users.noreply.github.com>
2020-05-11 11:00:14 +0200

    Variable test

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,7 @@
 const mix = require('laravel-mix');
+var sassVars = './scssVars.json';
 
 mix.js('site/themes/sugarpilots/js/site.js', 'site/themes/sugarpilots/js/sugarpilots.js')
-    .sass('site/themes/sugarpilots/sass/sugarpilots.scss', 'site/themes/sugarpilots/css');
+    .sass('site/themes/sugarpilots/sass/sugarpilots.scss', 'site/themes/sugarpilots/css', {
+        prependData: '$assets:\'' + process.env.MIX_ASSETS + '\';'
+    });
