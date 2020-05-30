e129da5e0e77ea83a26b5018a792c95618812276
Jannick Holm <jannickholm@users.noreply.github.com>
2020-05-30 11:19:31 +0200

    add to cart

--- site/themes/sugarpilots/js/site.js
+++ site/themes/sugarpilots/js/site.js
@@ -2,10 +2,45 @@
 import Rellax from 'rellax';
 import Vue from 'vue';
 import Vuex from 'vuex';
+import Scrollbar from 'smooth-scrollbar';
+import AOS from 'aos';
 Vue.use(Vuex);
 require("./cursor");
+AOS.init({
+    once: false,
+    mirror: false,
+    offset: 100
+});
 
-  // import all vue components
+Scrollbar.initAll({
+
+    // Momentum reduction damping factor, a float value between (0, 1)
+    damping: .1,
+  
+    // Minimal size for scrollbar thumb.
+    thumbMinSize: 20,
+  
+    // Render scrolling by integer pixels
+    renderByPixels: true,
+  
+    // Whether allow upper scrollable content to continue scrolling when current scrollbar reaches edge. 
+    // When set to 'auto', it will be enabled on nested scrollbars, and disabled on first-class scrollbars.
+    continuousScrolling: true,
+  
+    // Keep scrollbar tracks always visible.
+    alwaysShowTracks: false,
+  
+    // Element to be used as a listener for mouse wheel scroll events. 
+    // By default, the container element is used.
+    wheelEventTarget: null,
+  
+    // plugins
+    plugins: {}
+    
+  });
+
+
+// import all vue components
 const files = require.context('./', true, /\.vue$/i);
 files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default));
 
@@ -49,18 +84,25 @@ window.addEventListener("resize", () => {
     }
 })
 
+  
+    var section = document.querySelectorAll(".section");
+    var sections = {};
+    var i = 0;
+  
+    Array.prototype.forEach.call(section, function(e) {
+      sections[e.id] = e.offsetTop;
 
-document.querySelectorAll(".expand-cursor").forEach(button => {
-    button.addEventListener("mouseover", resizeCursor);
-    button.addEventListener("mouseout", resetCursor);
-});
-
-function resizeCursor() {
-    const cursor = document.querySelector(".cursor");
-    cursor.classList = "cursor link-hover";
-}
-
-function resetCursor() {
-    const cursor = document.querySelector(".cursor");
-    cursor.classList = "cursor";
-}
+      console.log(e.offsetTop)
+    });
+  
+    window.onscroll = function() {
+      var scrollPosition = document.documentElement.scrollTop || document.body.scrollTop;
+  
+      for (i in sections) {
+        if (sections[i] <= scrollPosition) {
+          document.querySelector('.active').setAttribute('class', ' ');
+          document.querySelector('a[href*=' + i + ']').setAttribute('class', 'active');
+          console.log(i)
+        }
+      }
+    };
--- webpack.mix.js
+++ webpack.mix.js
@@ -2,8 +2,6 @@ const mix = require('laravel-mix');
 const config = require('./webpack.config');
 
 mix.js('site/themes/sugarpilots/js/site.js', 'site/themes/sugarpilots/js/sugarpilots.js')
-    .sass('site/themes/sugarpilots/sass/sugarpilots.scss', 'site/themes/sugarpilots/css', {
-        prependData: '$assets:\'' + process.env.MIX_ASSETS + '\';'
-    });
+    .sass('site/themes/sugarpilots/sass/sugarpilots.scss', 'site/themes/sugarpilots/css');
 
 mix.webpackConfig(config);
